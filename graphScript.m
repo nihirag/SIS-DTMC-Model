56e20ccf013378cf10c1269425766aea41fadf2e
Nihir Agarwal <f20180701@pilani.bits-pilani.ac.in>
2020-07-14 21:25:54 +0530

    initial commit

---
+++ graphScript.m
@@ -0,0 +1,36 @@
+clear
+set(0,'DefaultAxesFontSize', 18)
+beta=0.0000002857;
+g=0.02011;
+b=0.00137363;
+R0=beta/(b+g);
+N=150000;
+init=10000;
+dt=0.01;
+time=5000;
+sim=3;
+for j=1:sim
+i(1)=init;
+for t=1:time/dt
+r=rand; % uniform random number
+birth=beta*i(t)*(N-i(t))/N*dt; 
+death=(b+g)*i(t)*dt;
+if r<=birth
+i(t+1)=i(t)+1;
+elseif r>birth && r<=birth+death
+i(t+1)=i(t)-1;
+else
+i(t+1)=i(t);
+end
+end
+if j==1
+    stairs([0:dt:time],i,'r-','LineWidth',2);
+hold on
+elseif j==2 
+    stairs([0:dt:time],i,'g-','LineWidth',2);
+else
+    stairs([0:dt:time],i,'b-','LineWidth',2);
+end
+end
+ylabel("Infected")
+xlabel("Time Steps")
